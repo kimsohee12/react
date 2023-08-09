a47bc938c7839330a345f502ca58e86910201640
김소희 <130417959+kimsohee12@users.noreply.github.com>
2023-08-09 23:57:01 +0900

    banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,46 @@
+import axios from '../api/axios';
+import React, { useEffect, useState } from 'react'
+import requests from '../api/requests';
+import './Banner.css'
+
+export const Banner = () => {
+
+    const [movie, setMovie] = useState([]);
+    const fetchData = async()=>{
+        //현재 상영중인 영화 정보 가져오기
+        const request = await axios.get(requests.fetchNowPlaying);
+        //여러 영화중 영화 하나의 id 가져오기
+        const movieId = request.data.results[
+            Math.floor(Math.random()*request.data.results.length)
+        ].id;
+    
+        //특정 영화의 더 상세한 정보를 가져오기 (비디오 정보포함)
+        const {data : movieDetail} = await axios.get(`movie/${movieId}`,{
+            params : {append_to_response:"videos"},
+        });
+        setMovie(movieDetail);
+    };
+    
+    useEffect(()=>{
+        fetchData();
+        console.log(movie);
+    },[]);
+
+  return (
+    <header className='banner' style={{
+        backgroundImage:`url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`, 
+        backgroundPosition:"top center",
+        backgroundSize:"cover"}}>
+            
+        <div className='banner_contents'>
+            <h1>{movie.title||movie.name||movie.orginal_name}</h1>
+            <div className='banner_buttons'>
+                <button className='banner_button play'>play</button>
+                <button className='banner_button info'>More Information</button>
+            </div>
+            <h1 className='banner_description'>{movie.overview}</h1>
+        </div>
+        <div className='banner--fadeBottom'/>
+    </header>
+  )
+}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logoImg from '../img/Netflix-Brand-Logo.png'
 import profileImg from "../img/netflixpfImg.jpg";
 import "./Nav.css"
 
 const Nav = () => {
+  const [show,setShow] = useState(false);
+
+  useEffect(()=>{
+    window.addEventListener("scroll",()=>{
+      console.log("scroll",window.scrollY);
+      if(window.scrollY > 50){
+        setShow(true);
+      }else{
+        setShow(false);
+      }
+    })
+    window.removeEventListener("scroll",()=>{})
+  },[])
     
   return (
-    <nav className='nav'>
+    <nav className={`nav ${show && "nav_Black"}`}>
         <img alt='Netflix logo'width={'150px'} src={logoImg} className='nav_log' onClick={()=>window.location.reload()}/>
         <img alt='User logged' src ={profileImg} className='nav_avatar'width={'50px'}/>
     </nav>
