6de380e7ac432fbc7150be1d649735b4e29442bf
김소희 <130417959+kimsohee12@users.noreply.github.com>
2023-08-11 00:30:59 +0900

    Row,Footer 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import { Banner } from './components/Banner';
+import Row from './components/Row';
+import requests from './api/requests';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
+    <div className='app'>
       <Nav/>
       <Banner/>
+      <Row title ="NETFLIX ORIGINALS" id="NO" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title ="Trending Now" id="TN" fetchUrl={requests.fetchTrending}/>
+      <Row title ="Top Rated" id="TR" fetchUrl={requests.fetchTrending}/>
+      <Row title ="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies}/>
+      <Row title ="Commedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies}/>
+      <Footer/>
     </div>
   );
 }
