3f50df0c9e87ce65302c863ab8bc416a0f37764f
김소희 <130417959+kimsohee12@users.noreply.github.com>
2023-08-13 02:19:56 +0900

    banner, css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,6 +8,7 @@ export const Banner = () => {
 
     const [movie, setMovie] = useState([]);
     const [isClicked, setIsClicked] = useState(false);
+    const [moreOV,setMoreOV] = useState(false);
 
     useEffect(() => {
         fetchData();
@@ -34,9 +35,17 @@ export const Banner = () => {
 
     //movie.overview(줄거리)가 길면 '...' 으로 자르기
     const truncate = (str, n) => {
-        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+        return str?.length > n ? str.substr(0, n - 1) + "... 더보기" : str;
     };
 
+    const videoBtnClick=()=>{
+        if(movie.videos.results.length==0){
+            alert('비디오 없음')
+        }else{
+            setIsClicked(true)
+
+        }
+    }
     console.log('movie', movie);
     if (!isClicked) {
         return (
@@ -55,15 +64,16 @@ export const Banner = () => {
                         <div className="banner__buttons">
                             <button
                                 className="banner__button play"
-                                onClick={() => setIsClicked(true)}
+                                onClick={() => videoBtnClick()}
                             >
                                 Play
                             </button>
-                            <button className="banner__button info">More Information</button>
+                            {/* <button className="banner__button info">More Information</button> */}
                         </div>
 
-                        <h1 className="banner__description">
-                            {truncate(movie.overview, 100)}
+                        <h1 className="banner__description" onClick={()=>setMoreOV(true)}>
+                    
+                    {moreOV? truncate(movie.overview, 600) :truncate(movie.overview, 100)}
                         </h1>
                     </div>
 
@@ -85,6 +95,7 @@ export const Banner = () => {
         );
     }
 }
+export default Banner
 
 const Iframe = styled.iframe`
     width: 100%;
